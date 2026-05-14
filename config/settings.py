e117cce28e06f2165dd672369b9ac261a24ba35c
Nikodimos Hunegn <nikodimoshunegn17@gmail.com>
2026-05-14 00:01:16 +0000

    Auto-configure ALLOWED_HOSTS on Render, default DEBUG to False

--- config/settings.py
+++ config/settings.py
@@ -6,9 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me-in-production")
 
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+if config("RENDER", default=False, cast=bool):
+    ALLOWED_HOSTS.append(config("RENDER_EXTERNAL_HOSTNAME"))
 
 INSTALLED_APPS = [
     "django.contrib.admin",
